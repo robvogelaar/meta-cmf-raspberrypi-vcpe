db7c6378cf2672679c4337956967931344f992b7
Sarveswaran Nagarajan <sanagarajan.contractor@libertyglobal.com>
2025-12-07 15:19:04 -0500

    Update all tdk config files as per latest community release

--- gen/configs/tdk/run_tomcat_mysql.sh
+++ gen/configs/tdk/run_tomcat_mysql.sh
@@ -1,34 +1,53 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2021 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+
 #!/bin/bash
-# Script to run Tomcat and MySQL services for TDK
 
-echo "Starting TDK services..."
-
-# Start MySQL service
-echo "Starting MySQL..."
-service mysql start
-sleep 5
-
-# Verify MySQL is running
-if ! pgrep -x "mysqld" > /dev/null; then
-    echo "Failed to start MySQL"
-    exit 1
+# Start the first process
+service mysql start -D
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start mysql: $status"
+  exit $status
 fi
-echo "MySQL started successfully"
 
-# Start Tomcat service
-echo "Starting Tomcat..."
-/etc/init.d/tomcat7 start
+#Wait for 10 seconds
+echo "Sleeping for 10 seconds"
 sleep 10
 
-# Verify Tomcat is running
-if ! pgrep -f "tomcat" > /dev/null; then
-    echo "Failed to start Tomcat"
-    exit 1
+# Start the second process
+/etc/init.d/tomcat7 start -D
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start tomcat: $status"
+  exit $status
 fi
-echo "Tomcat started successfully"
 
-echo "TDK services are running"
-echo "Access TDK Test Manager at: http://localhost:8080/rdk-test-tool"
-
-# Keep the container running
-tail -f /opt/apache-tomcat-7.0.96/logs/catalina.out
+while sleep 10; do
+  TOMCAT_PID=$(ps aux | grep org.apache.catalina.startup.Bootstrap | grep -v grep | awk '{ print $2 }')
+  MYSQL_PID=$(ps aux | grep /usr/sbin/mysqld | grep -v grep | awk '{ print $2 }')
+  if 
+  [ ! -n "$TOMCAT_PID" ] || 
+  [ ! -n "$MYSQL_PID" ]
+  then
+      echo "One of the processes has already exited."
+      echo "Tomcat PID : "$TOMCAT_PID
+      echo "Mysql PID : "$MYSQL_PID
+  fi
+done
--- gen/configs/tdk/war_creation_generic.py
+++ gen/configs/tdk/war_creation_generic.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-"""
 ##########################################################################
 # If not stated otherwise in this file or this component's Licenses.txt
 # file the following copyright and licenses apply:
@@ -19,91 +17,250 @@
 # limitations under the License.
 ##########################################################################
 
-Script to create rdk-test-tool.war from TDK repositories
-"""
+#!/usr/bin/python
 
 import os
 import sys
 import subprocess
-import shutil
 import time
+#import argparse
 
-def execute_command(command):
-    """Execute shell command and return output"""
-    try:
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        if result.returncode != 0:
-            print(f"Error executing command: {command}")
-            print(f"Error: {result.stderr}")
-            sys.exit(1)
-        return result.stdout
-    except Exception as e:
-        print(f"Exception executing command: {command}")
-        print(f"Exception: {str(e)}")
-        sys.exit(1)
 
-def main():
-    """Main function to create TDK war file"""
+try:
+    JAVA_VERSION = subprocess.Popen(['java','-version'])
+    out = JAVA_VERSION.communicate()
+    #version_name=str(sys.argv[0])
+    GRAILS_VERSION = subprocess.Popen(['grails','-version'])
+    out = GRAILS_VERSION.communicate()
+    current_directory = os.getcwd()
+
+    if(os.path.isdir(current_directory+'/FOLDER')):
+        subprocess.check_output(['rm','-rf','./FOLDER'])
+
+    subprocess.check_output(['mkdir','-p','FOLDER/B'])
+    subprocess.check_output(['mkdir','-p','FOLDER/ADV'])
+    subprocess.check_output(['mkdir','-p','FOLDER/C'])
+    #version_name
+    #if(len(sys.argv)==0): 
+    if((len(sys.argv))==1):
+    #cloning tdk generic code
+        #version=sys.argv[0]
+        os.chdir('./FOLDER')
+        #version_name = raw_input("Please enter release tag name? ")
+        #print("current tag name :".str(version_name))
+        clone_tdk_generic = subprocess.Popen(['git','clone','https://code.rdkcentral.com/r/rdk/tools/tdk'])
+        out = clone_tdk_generic.communicate()
+        #clone_tdk_generic = subprocess.Popen(['git','clone','-b','%s'%version_name,'https://code.rdkcentral.com/r/rdk/tools/tdk'])
     
-    # Get tag name from command line argument
-    if len(sys.argv) > 1:
-        tag_name = sys.argv[1]
+        #out = clone_tdk_generic.communicate()
+
+        #cloning tdk-b generic code
+        os.chdir('./B')
+        clone_tdk_b_generic = subprocess.Popen(['git','clone','https://code.rdkcentral.com/r/rdkb/tools/tdkb'])
+        out = clone_tdk_b_generic.communicate()
+        #clone_tdk_b_generic = subprocess.Popen(['git','clone','-b','%s'%version_name,'https://code.rdkcentral.com/r/rdkb/tools/tdkb'])
+        #out = clone_tdk_b_generic.communicate()
+
+        #cloning tdk-c generic code
+        os.chdir('../C')
+        clone_tdk_c_generic = subprocess.Popen(['git','clone','https://code.rdkcentral.com/r/rdkc/tools/tdkc'])
+        out = clone_tdk_c_generic.communicate()
+        #clone_tdk_c_generic = subprocess.Popen(['git','clone','-b','%s'%version_name,'https://code.rdkcentral.com/r/rdkc/tools/tdkc'])
+        #out = clone_tdk_c_generic.communicate()
+
+        os.chdir('..')
+        #subprocess.check_output(['cp','/home/jnode/Rahim/DOcker_test_setup/BuildConfig.groovy','./tdk/framework/grails-app/conf'])
+        subprocess.check_output(['mkdir','./tdk/framework/web-app/fileStore/testscriptsRDKB'])
+        subprocess.check_output(['mkdir','./tdk/framework/web-app/fileStore/testscriptsRDKC'])
+        subprocess.check_output(['cp','-r','./B/tdkb/testscripts/RDKB/component','./tdk/framework/web-app/fileStore/testscriptsRDKB'])
+        subprocess.check_output(['cp','-r','./C/tdkc/testscripts/RDKC/component','./tdk/framework/web-app/fileStore/testscriptsRDKC'])
+        
+        os.chdir('./tdk/framework/')
+        clean_out = subprocess.Popen(['grails','clean'])
+        out = clean_out.communicate()
+
+        #creating tdk war
+        war_out = subprocess.Popen(['grails','prod','war','rdk-test-tool.war'])
+        out = war_out.communicate()
+
+        current_directory = os.getcwd()
+
+        ls_tdk_framework = subprocess.Popen(['ls'])
+        out = ls_tdk_framework.communicate()
+
+        ls_tdk_mysql_database = subprocess.Popen(['ls','../mysql-database/'])
+        out = ls_tdk_mysql_database.communicate()
+
+        subprocess.check_output(['cp','./rdk-test-tool.war','/mnt'])
+        subprocess.check_output(['cp','../mysql-database/rdktestproddbdump.sql','/mnt'])
+
+        ls_mnt = subprocess.Popen(['ls','/mnt'])
+        out = ls_tdk_mysql_database.communicate()
+
+        print("rdk-test-tool.war annd rdktestproddbdump.sql copied to /mnt")
+    elif ((len(sys.argv))==2):
+        #version_name=str(sys.argv[1])
+        fileName=sys.argv[1]
+        if(fileName.startswith('RDKV_')): 
+            os.chdir('./FOLDER')
+             #version_name = raw_input("Please enter release tag name? ")
+            #print("current tag name :".str(version_name))
+            clone_tdk_generic = subprocess.Popen(['git','clone','-b','%s'%sys.argv[1],'https://code.rdkcentral.com/r/rdk/tools/tdk'])
+            out = clone_tdk_generic.communicate()
+
+            #cloning tdk-b generic code
+            #os.chdir('./B')
+            #clone_tdk_b_generic = subprocess.Popen(['git','clone','-b','%s'%sys.argv[1],'https://code.rdkcentral.com/r/rdkb/tools/tdkb'])
+            #out = clone_tdk_b_generic.communicate()
+
+            #cloning tdk-c generic code
+            #os.chdir('../C')
+            #clone_tdk_c_generic = subprocess.Popen(['git','clone','-b','%s'%sys.argv[1],'https://code.rdkcentral.com/r/rdkc/tools/tdkc'])
+            #out = clone_tdk_c_generic.communicate()
+
+            #os.chdir('..')
+            #subprocess.check_output(['cp','/home/jnode/Rahim/DOcker_test_setup/BuildConfig.groovy','./tdk/framework/grails-app/conf'])
+            #subprocess.check_output(['mkdir','./tdk/framework/web-app/fileStore/testscriptsRDKB'])
+            #subprocess.check_output(['mkdir','./tdk/framework/web-app/fileStore/testscriptsRDKC'])
+            #subprocess.check_output(['cp','-r','./B/tdkb/testscripts/RDKB/component','./tdk/framework/web-app/fileStore/testscriptsRDKB'])
+            #subprocess.check_output(['cp','-r','./C/tdkc/testscripts/RDKC/component','./tdk/framework/web-app/fileStore/testscriptsRDKC'])
+
+            os.chdir('./tdk/framework/')
+            clean_out = subprocess.Popen(['grails','clean'])
+            out = clean_out.communicate()
+
+            #creating tdk war
+            war_out = subprocess.Popen(['grails','prod','war','rdk-test-tool.war'])
+            out = war_out.communicate()
+
+            current_directory = os.getcwd()
+
+            ls_tdk_framework = subprocess.Popen(['ls'])
+            out = ls_tdk_framework.communicate()
+
+            ls_tdk_mysql_database = subprocess.Popen(['ls','../mysql-database/'])
+            out = ls_tdk_mysql_database.communicate()
+
+            subprocess.check_output(['cp','./rdk-test-tool.war','/mnt'])
+            subprocess.check_output(['cp','../mysql-database/rdktestproddbdump.sql','/mnt'])
+
+            ls_mnt = subprocess.Popen(['ls','/mnt'])
+            out = ls_tdk_mysql_database.communicate()
+
+            print("rdk-test-tool.war annd rdktestproddbdump.sql copied to /mnt")
+        else:
+            os.chdir('./FOLDER')
+            #version_name = raw_input("Please enter release tag name? ")
+            #print("current tag name :".str(version_name))
+            clone_tdk_generic = subprocess.Popen(['git','clone','-b','%s'%sys.argv[1],'https://code.rdkcentral.com/r/rdk/tools/tdk'])
+            out = clone_tdk_generic.communicate()
+
+
+
+            #cloning tdk-b generic code
+            os.chdir('./B')
+            clone_tdk_b_generic = subprocess.Popen(['git','clone','-b','%s'%sys.argv[1],'https://code.rdkcentral.com/r/rdkb/tools/tdkb'])
+            out = clone_tdk_b_generic.communicate()
+
+            #cloning tdk-c generic code
+            os.chdir('../C')
+            clone_tdk_c_generic = subprocess.Popen(['git','clone','-b','%s'%sys.argv[1],'https://code.rdkcentral.com/r/rdkc/tools/tdkc'])
+            out = clone_tdk_c_generic.communicate()
+
+            os.chdir('..')
+            subprocess.check_output(['mkdir','./tdk/framework/web-app/fileStore/testscriptsRDKB'])
+            subprocess.check_output(['mkdir','./tdk/framework/web-app/fileStore/testscriptsRDKC'])
+            subprocess.check_output(['cp','-r','./B/tdkb/testscripts/RDKB/component','./tdk/framework/web-app/fileStore/testscriptsRDKB'])
+            subprocess.check_output(['cp','-r','./C/tdkc/testscripts/RDKC/component','./tdk/framework/web-app/fileStore/testscriptsRDKC'])
+
+            os.chdir('./tdk/framework/')
+            clean_out = subprocess.Popen(['grails','clean'])
+            out = clean_out.communicate()
+
+            #creating tdk war
+            war_out = subprocess.Popen(['grails','prod','war','rdk-test-tool.war'])
+            out = war_out.communicate()
+
+            current_directory = os.getcwd()
+
+            ls_tdk_framework = subprocess.Popen(['ls'])
+            out = ls_tdk_framework.communicate()
+
+            ls_tdk_mysql_database = subprocess.Popen(['ls','../mysql-database/'])
+            out = ls_tdk_mysql_database.communicate()
+            subprocess.check_output(['cp','./rdk-test-tool.war','/mnt'])
+            subprocess.check_output(['cp','../mysql-database/rdktestproddbdump.sql','/mnt'])
+
+            ls_mnt = subprocess.Popen(['ls','/mnt'])
+            out = ls_tdk_mysql_database.communicate()
+            print("rdk-test-tool.war annd rdktestproddbdump.sql copied to /mnt")
     else:
-        tag_name = "rdk-next"
-    
-    print(f"Creating TDK war file for tag: {tag_name}")
-    
-    # Change to /mnt directory
-    os.chdir("/mnt")
-    
-    # Clone TDK repositories
-    print("Cloning TDK-UI repository...")
-    execute_command(f"git clone https://code.rdkcentral.com/r/rdk/tools/tdk/TDK-UI -b {tag_name}")
-    
-    print("Cloning TDK repository...")
-    execute_command(f"git clone https://code.rdkcentral.com/r/rdk/tools/tdk -b {tag_name}")
-    
-    # Change to TDK-UI directory
-    os.chdir("/mnt/TDK-UI")
-    
-    # Copy required files
-    print("Copying TDK framework files...")
-    shutil.copytree("/mnt/tdk/framework", "framework")
-    
-    # Build configuration
-    print("Building TDK Test Manager...")
-    
-    # Set environment variables
-    os.environ['JAVA_HOME'] = '/usr/lib/jvm/java-8-openjdk-amd64'
-    os.environ['GRAILS_HOME'] = '/usr/lib/jvm/grails-2.4.0'
-    os.environ['PATH'] = f"{os.environ['GRAILS_HOME']}/bin:{os.environ['JAVA_HOME']}/bin:{os.environ['PATH']}"
-    
-    # Clean and compile
-    print("Running grails clean...")
-    execute_command("grails clean")
-    
-    print("Running grails compile...")
-    execute_command("grails compile")
-    
-    print("Running grails war...")
-    execute_command("grails war rdk-test-tool.war")
-    
-    # Copy war file to /mnt
-    print("Copying war file...")
-    shutil.copy("rdk-test-tool.war", "/mnt/rdk-test-tool.war")
-    
-    # Copy SQL dump if exists
-    sql_dump = "framework/grails-app/utils/rdktestproddbdump.sql"
-    if os.path.exists(sql_dump):
-        print("Copying SQL dump...")
-        shutil.copy(sql_dump, "/mnt/rdktestproddbdump.sql")
-    
-    print("TDK war creation completed successfully!")
-    
-    # Clean up
-    os.chdir("/mnt")
-    shutil.rmtree("/mnt/TDK-UI", ignore_errors=True)
-    shutil.rmtree("/mnt/tdk", ignore_errors=True)
+        filename=sys.argv[2]
+        if(filename=="license=advanced") or (filename=="advanced"):
+            #cloning tdk generic code
+            os.chdir('./FOLDER')
+           #clone_tdk_generic = subprocess.Popen(['git','clone','https://code.rdkcentral.com/r/rdk/tools/tdk'])
+            clone_tdk_generic = subprocess.Popen(['git','clone','-b','%s'%sys.argv[1],'https://code.rdkcentral.com/r/rdk/tools/tdk'])
+            out = clone_tdk_generic.communicate()
 
-if __name__ == "__main__":
-    main()
+            #cloning tdk-b generic code
+            os.chdir('./B')
+           #clone_tdk_b_generic = subprocess.Popen(['git','clone','https://code.rdkcentral.com/r/rdkb/tools/tdkb'])
+            clone_tdk_b_generic = subprocess.Popen(['git','clone','-b','%s'%sys.argv[1],'https://code.rdkcentral.com/r/rdkb/tools/tdkb'])
+            out = clone_tdk_b_generic.communicate()
+
+            #cloning tdk-c generic code
+            os.chdir('../C')
+           # clone_tdk_c_generic = subprocess.Popen(['git','clone','https://code.rdkcentral.com/r/rdkc/tools/tdkc'])
+            clone_tdk_c_generic = subprocess.Popen(['git','clone','-b','%s'%sys.argv[1],'https://code.rdkcentral.com/r/rdkc/tools/tdkc'])
+            out = clone_tdk_c_generic.communicate()
+    
+            #cloning adv generic code
+            os.chdir('../ADV')
+           # clone_tdk_adv_generic = subprocess.Popen(['git','clone','https://code.rdkcentral.com/r/tools/tdk-advanced'])
+            clone_tdk_adv_generic = subprocess.Popen(['git','clone','-b','%s'%sys.argv[1],'https://code.rdkcentral.com/r/tools/tdk-advanced'])
+            out = clone_tdk_adv_generic.communicate()
+   
+   
+            os.chdir('..')
+            subprocess.check_output(['mkdir','./tdk/framework/web-app/fileStore/testscriptsRDKB'])
+            subprocess.check_output(['mkdir','./tdk/framework/web-app/fileStore/testscriptsRDKC'])
+            subprocess.check_output(['cp','-r','./B/tdkb/testscripts/RDKB/component','./tdk/framework/web-app/fileStore/testscriptsRDKB'])
+            subprocess.check_output(['cp','-r','./C/tdkc/testscripts/RDKC/component','./tdk/framework/web-app/fileStore/testscriptsRDKC'])
+            subprocess.check_output(['cp','-r','./ADV/tdk-advanced/framework/web-app/fileStore/','./tdk/framework/web-app/'])
+            subprocess.check_output(['rm','-rf','./tdk/framework/web-app/fileStore/testscriptsTCL'])
+    
+    
+            os.chdir('./tdk/framework/')
+            clean_out = subprocess.Popen(['grails','clean'])
+            out = clean_out.communicate()
+
+            #creating tdk war
+            war_out = subprocess.Popen(['grails','prod','war','rdk-test-tool.war'])
+            out = war_out.communicate()
+
+            current_directory = os.getcwd()
+
+            ls_tdk_framework = subprocess.Popen(['ls'])
+            out = ls_tdk_framework.communicate()
+
+            ls_tdk_mysql_database = subprocess.Popen(['ls','../mysql-database/'])
+            out = ls_tdk_mysql_database.communicate()
+
+            subprocess.check_output(['cp','./rdk-test-tool.war','/mnt'])
+            subprocess.check_output(['cp','../mysql-database/rdktestproddbdump.sql','/mnt'])
+
+            ls_mnt = subprocess.Popen(['ls','/mnt'])
+            out = ls_tdk_mysql_database.communicate()
+
+            print("rdk-test-tool.war annd rdktestproddbdump.sql copied to /mnt")
+            
+        else:
+            print("Please mention license tag for Advaned repo contents")
+    #   filename = sys.argv[1]
+    #   args = dict([arg.split('=', maxsplit=1) for arg in sys.argv[2:]])
+    #   print "key value"
+except:
+    e = sys.exc_info()[1]
+    print("Error occured : " + str(e))
+    sys.exit()
